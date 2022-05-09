e8ae7db54728d4d061c5a22c43ea6cd7ba32c360
xizh <995194437@qq.com>
2022-05-09 18:49:31 +0800

    feat(redux): 添加redux作为状态管理工具

---
+++ src/reducers/song.ts
@@ -0,0 +1,6 @@
+export default function song(state = {}, action){
+    switch(action.type){
+        default:
+            return state
+    }
+}
---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleWare from "redux-thunk";
+import rootReducers from "../reducers";
+
+const composeEnhancers =
+  typeof window === "object" &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+const middleWares = [
+    thunkMiddleWare,
+]
+
+if(process.env.NODE_ENV === 'development'){
+    middleWares.push(require('redix-logger').createLogger())
+}
+
+const enhancer = composeEnhancers(
+    applyMiddleware(...middleWares),
+)
+
+export default function configStore(){
+    const store = createStore(rootReducers, enhancer)
+    return store
+}
